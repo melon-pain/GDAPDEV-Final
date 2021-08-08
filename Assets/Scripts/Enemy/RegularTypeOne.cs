d567d58394452695c25b6e05e747eec2d0005463
John Santos <john_adriel_santos@dlsu.edu.ph>
2021-08-08 22:52:24 +0800

    Enemies have elements. Can only be defeated with its weakness.

--- Assets/Scripts/Enemy/RegularTypeOne.cs
+++ Assets/Scripts/Enemy/RegularTypeOne.cs
@@ -2,35 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-enum EnemyElementType
-{
-    Fire,
-    Water,
-    Electric,
-    Ice
-}
-
 public class RegularTypeOne : MonoBehaviour
 {
     EnergySystem energy;
     private float energyMax = 100f;
     private Bar energyBar;
 
-    [SerializeField] private EnemyElementType elementType;
-    [SerializeField] private GameObject model;
+    [SerializeField] private Element element;
+    [SerializeField] private GameObject plate;
+    [SerializeField] private List<Material> materials = new List<Material>();
     private float rotationSpeed;
     // Start is called before the first frame update
     void Start()
     {
+        element = (Element)Random.Range(0, 4);
         energy = new EnergySystem(energyMax);
         energyBar = this.transform.gameObject.GetComponentInChildren<Bar>();
         rotationSpeed = Random.Range(50f, 100f);
+        plate.GetComponent<MeshRenderer>().material = materials[(int)element];
+    }
+
+    private void OnValidate()
+    {
+        plate.GetComponent<MeshRenderer>().material = materials[(int)element];
     }
 
     // Update is called once per frame
     void Update()
     {
-        model.transform.Rotate(new Vector3(0f, rotationSpeed, 0f) * Time.deltaTime);
+        plate.transform.Rotate(new Vector3(0f, rotationSpeed, 0f) * Time.deltaTime);
         if(Input.GetKeyDown(KeyCode.R))
         {
             DamageTaken(20f);
@@ -45,7 +45,8 @@ public class RegularTypeOne : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        DamageTaken(20f);
+        if(collision.collider.GetComponent<Projectile>().GetElement() == Elements.GetWeakness(this.element))
+            DamageTaken(20f);
     }
 
     private void DamageTaken(float amount)
